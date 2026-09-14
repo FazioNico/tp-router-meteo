b6c1fc783763bf19ab1a35a2b60fbc8ac29998fb
FazioNico <contact@nicolasfazio.ch>
2026-09-14 12:17:07 +0200

    fix: update API_URL to VITE_API_URL in fetchMeteoData function

--- src/services/MeteoService.jsx
+++ src/services/MeteoService.jsx
@@ -2,7 +2,7 @@
 export const fetchMeteoData = async (city) => {
   // Open Meteo API doc: https://open-meteo.com/en/docs/geocoding-api
   // 1: use geocoding to get lat lng from city name
-  const geocodingResponse = await fetch(`${import.meta.env.API_URL}/v1/search?name=${city}`);
+  const geocodingResponse = await fetch(`${import.meta.env.VITE_API_URL}/v1/search?name=${city}`);
   const geocodingData = await geocodingResponse.json();
   if (!geocodingData.results || geocodingData.results.length === 0) {
     throw new Error(`No results found for city: ${city}`);
@@ -10,7 +10,7 @@ export const fetchMeteoData = async (city) => {
   const { latitude, longitude } = geocodingData.results[0];
   // Open Meteo API doc: https://open-meteo.com/en/docs/geocoding-api
   // 2: use lat lng to get weather data
-  const meteoResponse = await fetch(`${import.meta.env.API_URL}/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true`);
+  const meteoResponse = await fetch(`${import.meta.env.VITE_API_URL}/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true`);
   const meteoData = await meteoResponse.json();
   return meteoData;
 };
